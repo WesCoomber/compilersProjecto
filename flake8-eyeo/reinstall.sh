e26fad5bc00a92eedd6ed7c804e34bb5331c3783
swsprec <swsprec@umich.edu>
2017-12-03 17:46:59 -0500

    small changes to reinstall

--- flake8-eyeo/reinstall.sh
+++ flake8-eyeo/reinstall.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo 'sudo pip uninstall --yes flake8-eyeo'
-sudo pip uninstall --yes flake8-eyeo
-echo 'sudo pip install .'
-sudo pip install .
+UNINSTALL="sudo pip uninstall --yes flake8-eyeo"
+INSTALL="sudo pip install ."
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (with sudo)"
+  exit
+fi
+
+
+echo $UNINSTALL && eval $UNINSTALL
+
+echo $INSTALL && eval $INSTALL
+
